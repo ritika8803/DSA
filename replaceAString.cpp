2e97147ecd6bbc301eeaf137a2f4a9f57daf1f3a
ritika8803 <ritika.bhardwaj2021@vitbhopal.ac.in>
2024-01-13 14:04:08 +0530

    replace a string code added

---
+++ replaceAString.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void replaceSpaces(char sentence[]){
+    int i =0;
+    int n = strlen(sentence);
+    for(int i =0; i<n ;i++){
+        if(sentence[i] == ' '){
+            sentence[i] = '@';
+        }
+    }
+}
+
+int main()
+{
+    char sentence[100];
+    cin.getline(sentence, 100);
+
+    replaceSpaces(sentence);
+    cout << "print the sentence with@ : " << endl << sentence << endl;
+  return 0;
+}

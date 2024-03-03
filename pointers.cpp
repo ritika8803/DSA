5c85318a9297af0dd3e998f7092b926a812f9426
ritika8803 <ritika.bhardwaj2021@vitbhopal.ac.in>
2024-03-03 12:09:20 +0530

    "today we learn about the pointers in array and char"

---
+++ pointers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int arr[4] = {12,14,16,18};
+
+    // cout << arr << " "<< arr[0] << " "<< &arr << &arr[0] << endl;
+    // cout << *arr << " " << arr[0] << "same " << endl;
+    // int *p = arr;
+    // cout << p << " "<< &p<< endl;
+
+    // int arr[4] = {5,6,7,8};
+
+    // int *p = arr;
+    // int *q = arr+1;
+
+    // cout << arr << endl; // address of a ---> 104
+    // cout << &arr << endl; // address of arr ---> 104
+    // cout << arr[0] << endl; // 5
+    // cout << &arr[0] << endl; // address of arr ---> 104
+    // cout << p << endl; //address of arr ---> 104
+    // cout << &p << endl; // address of p --> 208
+    // cout << *p << endl; //5
+    // cout << q << endl; //address of a + 1 ---> 108(4 bytes will increase)
+    // cout << &q << endl; //address of q --> 316
+    // cout << *q << endl; //6
+    // cout << *(p) + 1 << endl; //6
+    // cout << *(p) + 2 << endl; //7
+    // cout << *(q) + 3 << endl; //9
+    // cout << *(q) + 4 << endl; //10 
+    // cout << sizeof(p) << endl; // both depend on the system archi of the system 
+    // cout << sizeof(*p) << endl;
+
+    //implementation of pointer and cout is different for char and int 
+
+     
+
+  return 0;
+}
